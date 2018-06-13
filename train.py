ed14bc4700e47d24cd6adcf38ddc453c866fb7da
Cheng-Lin Li <clark.cl.li@gmail.com>
2018-06-13 08:18:09 -0700

    reduce epoch to 1 for MVP

--- train.py
+++ train.py
@@ -162,7 +162,7 @@ def train(args, train_list, val_list, u_model, net_input_shape):
                                              batchSize=args.batch_size,  numSlices=args.slices, subSampAmt=0,
                                              stride=20, shuff=args.shuffle_data),
         validation_steps=5, # Set validation stride larger to see more of the data.
-        epochs=2,
+        epochs=1,
         callbacks=callbacks,
         verbose=1)
     # Plot the training data collected
