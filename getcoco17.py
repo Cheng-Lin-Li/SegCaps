ceb4e2f8a1bd21080fce99197630c4462032f3dd
Cheng-Lin Li <clark.cl.li@gmail.com>
2018-06-14 23:52:34 -0700

    revise parameter name

--- getcoco17.py
+++ getcoco17.py
@@ -26,8 +26,6 @@ Currently focus on person category data.
 
 '''
 
-from __future__ import print_function
-
 import argparse
 
 from pycocotools.coco import COCO
@@ -44,13 +42,13 @@ MASK_FOLDER = 'masks'
 def main(args):
     pylab.rcParams['figure.figsize'] = (8.0, 10.0)
     plt.ioff()
-
+    
     data_dir = args.data_root_dir
-    category_list = args.category
-    annFile = args.annotation_dir    
+    category_list = list(args.category)
+    annFile = args.annotation_file   
     num = args.number
     file_name = ''
-
+ 
     #Create path for output
     output_image_path = data_dir+'/'+IMAGE_FOLDER
     if not os.path.isdir(output_image_path):
@@ -58,39 +56,41 @@ def main(args):
     output_mask_path = data_dir+'/'+MASK_FOLDER   
     if not os.path.isdir(output_mask_path):
         os.makedirs (output_mask_path)    
-
+ 
     # initialize COCO api for instance annotations
     coco=COCO(annFile)
-    
+     
     # get all images containing given categories, select one at random
-    catIds = coco.getCatIds(catNms=['person']);
-
+    catIds = coco.getCatIds(catNms=category_list);
+ 
     # Get image id list from categories.
     imgIds = coco.getImgIds(catIds=catIds );
     print('Image Generating...')
     for i in tqdm(range(num)):
         img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
-    
+     
         # use url to load image
         I = io.imread(img['coco_url'])
         plt.axis('off')
         file_name = output_image_path+'/'+FILE_MIDDLE_NAME+str(i)+'.png'
         plt.imsave(file_name, I)
-        
+         
         # Get annotation
         annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
         anns = coco.loadAnns(annIds)
         mask = coco.annToMask(anns[0])
-        
+         
         # Generate mask
         for j in range(len(anns)):
-            mask += coco.annToMask(anns[j])
-        
+             mask += coco.annToMask(anns[j])
+         
         # Background color = (R,G,B)=(68, 1, 84)
         # save the mask image
         file_name = output_mask_path+'/m_'+FILE_MIDDLE_NAME+str(i)+'.png'
         plt.imsave(file_name, mask)
+         
     print('Image Generation Complete !')
+    return True
 
 if __name__ == '__main__':
     '''
@@ -104,7 +104,7 @@ if __name__ == '__main__':
                         help='The root directory for your data.')
     parser.add_argument('--category', nargs='+', type=str, default='person',
                         help='MS COCO object categories list (--category person dog cat). default value is person')
-    parser.add_argument('--annotation_dir', type=str, default='./instances_val2017.json',
+    parser.add_argument('--annotation_file', type=str, default='./instances_val2017.json',
                         help='The annotation json file directory of MS COCO object categories list. file name should be instances_val2017.json')    
     parser.add_argument('--number', type=int, default=10,
                         help='The total number of images you want to download.')    
