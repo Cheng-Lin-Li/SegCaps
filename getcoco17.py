af66ccd2a7eea9ef149bc3211bc661c4ae58cc8d
Cheng-Lin Li <clark.cl.li@gmail.com>
2018-06-19 23:12:37 -0700

    change output image and mask size to 512X512 pix

--- getcoco17.py
+++ getcoco17.py
@@ -34,10 +34,42 @@ import skimage.io as io
 import matplotlib.pyplot as plt
 import pylab, os
 from tqdm import tqdm
+import cv2
 
 FILE_MIDDLE_NAME = 'train'
 IMAGE_FOLDER = 'imgs'
 MASK_FOLDER = 'masks'
+RESOLUTION = 512 # Resolution of the input for the model.
+
+def image_resize2square(image, desired_size = None):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    old_size = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if desired_size is None:
+        return image
+
+    # calculate the ratio of the height and construct the
+    # dimensions
+    ratio = float(desired_size)/max(old_size)
+    new_size = tuple([int(x*ratio) for x in old_size])
+
+    # new_size should be in (width, height) format
+    resized = cv2.resize(image, (new_size[1], new_size[0]))
+
+    delta_w = desired_size - new_size[1]
+    delta_h = desired_size - new_size[0]
+    top, bottom = delta_h//2, delta_h-(delta_h//2)
+    left, right = delta_w//2, delta_w-(delta_w//2)
+
+    color = [0, 0, 0]
+    new_image = cv2.copyMakeBorder(resized, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+
+    # return the resized image
+    return new_image
 
 def main(args):
 #     pylab.rcParams['figure.figsize'] = (8.0, 10.0)
@@ -71,6 +103,7 @@ def main(args):
      
         # use url to load image
         I = io.imread(img['coco_url'])
+        I = image_resize2square(I, RESOLUTION)
         plt.axis('off')
         file_name = join(output_image_path, FILE_MIDDLE_NAME+str(i)+'.png')
         plt.imsave(file_name, I)
@@ -86,6 +119,7 @@ def main(args):
          
         # Background color = (R,G,B)=[68, 1, 84]
         # save the mask image
+        mask = image_resize2square(mask, RESOLUTION)
         file_name = join(output_mask_path, FILE_MIDDLE_NAME+str(i)+'.png')
         plt.imsave(file_name, mask)
          
