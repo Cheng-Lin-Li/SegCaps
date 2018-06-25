d161c2df6937740ceef1dff44a7535c02c2171d1
Cheng-Lin Li <clark.cl.li@gmail.com>
2018-06-24 22:34:45 -0700

    installation shell script for Raspberry Pi 3

---
+++ opencv-install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Install Open CV"
+
+sudo apt-get install build-essential cmake pkg-config
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install libgtk2.0-dev libgtk-3-dev
+sudo apt-get install libatlas-base-dev gfortran
+
+wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.1.zip
+
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.1.zip
+
+unzip opencv.zip
+
+unzip opencv_contrib.zip
+
+cd ./opencv-3.4.1/
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/SegCaps/opencv_contrib-3.4.1/modules \
+    -D BUILD_EXAMPLES=ON ..
+
+make -j4
+
+sudo make install
+sudo ldconfig
+sudo apt-get update
+
+cd /usr/local/lib/python3.5/site-packages/
+sudo mv cv2.cpython-35m-arm-linux-gnueabihf.so cv2.so
+python -c "import cv2 as cv2; print(cv2.__version__)"
+
+
