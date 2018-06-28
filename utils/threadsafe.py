93d6f8ac50464bd1e46e33cbb7dbe5eb57088232
Cheng-Lin Li <clark.cl.li@gmail.com>
2018-06-27 23:56:18 -0700

    new module for functions aggregation

---
+++ utils/threadsafe.py
@@ -0,0 +1,39 @@
+'''
+Capsules for Object Segmentation (SegCaps)
+Original Paper by Rodney LaLonde and Ulas Bagci (https://arxiv.org/abs/1804.04241)
+Code written by: Rodney LaLonde
+If you use significant portions of this code or the ideas from our paper, please cite it :)
+If you have any questions, please email me at lalonde@knights.ucf.edu.
+
+This file is used for loading training, validation, and testing data into the models.
+It is specifically designed to handle 3D single-channel medical data.
+Modifications will be needed to train/test on normal 3-channel images.
+
+
+'''
+import threading
+
+''' Make the generators threadsafe in case of multiple threads '''
+class threadsafe_iter:
+    """Takes an iterator/generator and makes it thread-safe by
+    serializing call to the `next` method of given iterator/generator.
+    """
+    def __init__(self, it):
+        self.it = it
+        self.lock = threading.Lock()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        with self.lock:
+            return self.it.__next__()
+
+
+def threadsafe_generator(f):
+    """A decorator that takes a generator function and makes it thread-safe.
+    """
+    def g(*a, **kw):
+        return threadsafe_iter(f(*a, **kw))
+    return g
+
